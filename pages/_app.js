c7f18c3c508666aeff85504407ca19618b99782c
UDAY SANKAR MUKHERJEE <111909529+UdaySMukherjee@users.noreply.github.com>
2024-01-07 19:55:53 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,7 @@ function MyApp({ Component, pageProps }) {
   return(
   <Layout>
     <AnimatePresence mode='wait'>
-      <motion.div key={router.route} className='h-full overflow-y-full'>
+      <motion.div key={router.route} className='h-full overflow-y-scroll'>
         <Transition/>
       <Component {...pageProps} />;
       </motion.div>
