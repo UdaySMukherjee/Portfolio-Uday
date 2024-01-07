c0cb6d228f9b384c44a6dff5ef6247a0b26956a3
UDAY SANKAR MUKHERJEE <111909529+UdaySMukherjee@users.noreply.github.com>
2024-01-07 19:46:25 +0530

    Add files via upload

---
+++ pages/_app.js
@@ -0,0 +1,25 @@
+import '../styles/globals.css';
+
+import Layout from '../components/Layout';
+import Transition from '../components/Transition';
+
+import { useRouter } from 'next/router';
+
+//framer motion
+import { AnimatePresence, motion } from 'framer-motion';
+
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  return(
+  <Layout>
+    <AnimatePresence mode='wait'>
+      <motion.div key={router.route} className='h-full overflow-y-full'>
+        <Transition/>
+      <Component {...pageProps} />;
+      </motion.div>
+    </AnimatePresence>
+  </Layout>
+  );
+}
+
+export default MyApp;
